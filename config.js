4b33b897036fa3b0d69f93a54854d53f9c01ad2e
hemant <hemant.vedak@capgemini.com>
2017-04-07 07:59:39 +0530

    defect fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'MYSECRETKEY007',
-    'database':'mongodb://localhost:27017/elearnfinal2',
+    //'database':'mongodb://localhost:27017/elearnfinal2',
     'database':'mongodb://hemant1:Hemant1@ds157469.mlab.com:57469/elearndb',
     'PORT1':process.env.PORT || 3001
 };
